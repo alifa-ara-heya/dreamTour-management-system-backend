4ab75bd5b5d05c87ca71ee2a76b580952a5796e4
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-07-09 00:33:20 -0400

    setup completed

---
+++ src/server.ts
@@ -0,0 +1,82 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+
+import mongoose from 'mongoose';
+import app from "./app";
+import { envvars } from "./app/config/env";
+
+let server: Server;
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envvars.DB_URL)
+
+        console.log('Connected to MongoDB');
+
+        server = app.listen(5000, () => {
+            console.log(`Server is listening to port ${envvars.PORT}`);
+        })
+    } catch (error) {
+        console.log('error connecting to mongoDB', error);
+    }
+}
+
+startServer();
+
+// unhandled rejection error
+process.on("unhandledRejection", (err) => {
+    console.log('Unhandled Rejection detected! Server shutting down..', err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+
+// unhandled exception error
+process.on("uncaughtException", (err) => {
+    console.log('Uncaught Exception detected! Server shutting down..', err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+// throw new Error("I forgot to handle this local error.")
+
+
+// signal termination error- the server owner causes this error, such as for maintenance
+process.on("SIGTERM", (err) => {
+    console.log('Sigterm signal received! Server shutting down..', err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", (err) => {
+    console.log('Sigint signal received! Server shutting down..', err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
