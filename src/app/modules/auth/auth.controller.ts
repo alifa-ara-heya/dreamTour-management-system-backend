6ae2591575887fee0c8c8c71c14693623849b19c
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-07-16 20:24:21 -0400

    feat: implement setAuthCookie utility and refactor authentication flow to use it for setting tokens

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,21 +4,26 @@ import { sendResponse } from "../../utils/sendResponse"
 import { NextFunction, Request, Response } from 'express';
 import { AuthServices } from './auth.service';
 import AppError from '../../errorHelpers/appError';
+import { setAuthCookie } from '../../utils/setCookie';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    res.cookie("accessToken", loginInfo.accessToken, {
-        httpOnly: true, //ensures that these cookies cannot be accessed via client-side JavaScript, which helps protect them from cross-site scripting (XSS) attacks.
-        secure: false //means the cookies will be sent over both HTTP and HTTPS connections. In production environments, it is recommended to set secure: true so that cookies are only sent over secure HTTPS connections, further protecting sensitive authentication tokens.
-    })
+    /*   res.cookie("accessToken", loginInfo.accessToken, {
+          httpOnly: true, //ensures that these cookies cannot be accessed via client-side JavaScript, which helps protect them from cross-site scripting (XSS) attacks.
+          secure: false //means the cookies will be sent over both HTTP and HTTPS connections. In production environments, it is recommended to set secure: true so that cookies are only sent over secure HTTPS connections, further protecting sensitive authentication tokens.
+      }) */
 
-    res.cookie('refreshToken', loginInfo.refreshToken, {
-        httpOnly: true,
-        secure: false
-    })
+    /* 
+        res.cookie('refreshToken', loginInfo.refreshToken, {
+            httpOnly: true,
+            secure: false
+        }) */
+
+    setAuthCookie(res, loginInfo)
+
 
     sendResponse(res, {
         success: true,
@@ -37,6 +42,11 @@ const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: N
         throw new AppError(httpStatus.BAD_REQUEST, "No refresh token received from cookies.")
     }
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
+
+    res.cookie("accessToken", tokenInfo.accessToken, {
+        httpOnly: true,
+        secure: false
+    })
 
 
     sendResponse(res, {
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,15 +1,15 @@
 import AppError from "../../errorHelpers/appError"
 import httpStatus from 'http-status-codes';
-import { IsActive, IUser } from "../user/user.interface"
+import { /* IsActive,  */ IUser } from "../user/user.interface"
 import { User } from "../user/user.model"
 import bcryptjs from 'bcryptjs'
 // import jwt from 'jsonwebtoken'
 // import { generateToken } from "../../utils/jwt";
 // import { envVars } from "../../config/env";
-import { createUserTokens } from "../../utils/userTokens";
-import { generateToken, verifyToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
-import { JwtPayload } from "jsonwebtoken";
+import { createNewAccessTokenWithRefreshToken, createUserTokens } from "../../utils/userTokens";
+// import { generateToken, verifyToken } from "../../utils/jwt";
+// import { envVars } from "../../config/env";
+// import { JwtPayload } from "jsonwebtoken";
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload
@@ -55,7 +55,7 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
 
 }
 const getNewAccessToken = async (refreshToken: string) => {
-    const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload;
+    /* const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload;
 
     const isUserExist = await User.findOne({ email: verifiedRefreshToken.email })
     console.log('isUserExist', isUserExist);
@@ -78,10 +78,12 @@ const getNewAccessToken = async (refreshToken: string) => {
         role: isUserExist.role
     }
 
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
+    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES) */
+
+    const newAccessToken = createNewAccessTokenWithRefreshToken(refreshToken)
 
     return {
-        accessToken
+        accessToken: newAccessToken
     }
 
 }
