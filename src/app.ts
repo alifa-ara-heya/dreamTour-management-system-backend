b5a0209b336d05f4d27cc0ea7c5faf4bfb3f0842
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-07-18 16:39:21 -0400

    feat: add express-session and passport for user authentication, update environment variables for Google OAuth

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,18 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notfound";
 import cookieParser from 'cookie-parser';
+import passport from "passport";
+import expressSession from 'express-session';
 
 
 const app = express();
-
+app.use(expressSession({
+    secret: "Your Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors())
