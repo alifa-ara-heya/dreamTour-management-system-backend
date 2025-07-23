f4467f3dcf62e6d06afab9efb2cfc75d6199f6ab
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-07-22 21:54:58 -0400

    refactor: enhance slug generation logic in tour schema middleware

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -39,26 +39,30 @@ const tourSchema = new Schema<ITour>({
 })
 
 
-tourSchema.pre("save", async function (next) {
+tourSchema.pre("save", async function (next) //document middleware 
+{
 
-    if (this.isModified("title")) {
+    if (this.isModified("title")) { //checks if the title field has been changed. This is an efficient way to ensure the slug is only regenerated when the title changes, not on every save.
         const baseSlug = this.title.toLowerCase().split(" ").join("-")
         let slug = `${baseSlug}`
 
         let counter = 0;
-        while (await Tour.exists({ slug })) {
-            slug = `${slug}-${counter++}` // dhaka-division-2
+        while (await Tour.exists({ slug })) //to check if a document with the generated slug already exists in the database. 
+        {
+            slug = `${slug}-${counter++}` // If a duplicate is found, it appends a counter to the slug (e.g., dhaka-division-0, my-awesome-tour-1) and checks again until a unique slug is found.
         }
 
-        this.slug = slug;
+        this.slug = slug; //assigns the final, unique slug to the document's slug property before it's saved.
     }
     next()
 })
 
-tourSchema.pre("findOneAndUpdate", async function (next) {
-    const tour = this.getUpdate() as Partial<ITour>
+tourSchema.pre("findOneAndUpdate", async function (next) //query middleware
+{
+    const tour = this.getUpdate() as Partial<ITour> //retrieves the update object from the query.
 
-    if (tour.title) {
+    if (tour.title)  //checks if the title is being updated.
+    {
         const baseSlug = tour.title.toLowerCase().split(" ").join("-")
         let slug = `${baseSlug}`
 
@@ -71,7 +75,7 @@ tourSchema.pre("findOneAndUpdate", async function (next) {
         tour.slug = slug
     }
 
-    this.setUpdate(tour)
+    this.setUpdate(tour) //applies the modified update object back to the query, ensuring the new slug is included in the database operation.
 
     next()
 })
